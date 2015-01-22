d737058c4647167703140362a5e566fab0c8612f
Masaru Takano <mtaka@gclue.jp>
2015-01-22 10:37:39 +0900

    Implement Availability API.

--- dConnectSDK/dConnectSDKForIOS/DConnectSDK/Headers/DConnectSDK.h
+++ dConnectSDK/dConnectSDKForIOS/DConnectSDK/Headers/DConnectSDK.h
@@ -59,6 +59,7 @@
 #import <DConnectSDK/DConnectDeviceOrientationProfile.h>
 #import <DConnectSDK/DConnectConnectProfile.h>
 #import <DConnectSDK/DConnectBatteryProfile.h>
+#import <DConnectSDK/DConnectAvailabilityProfile.h>
 #import <DConnectSDK/DConnectAuthorizationProfile.h>
 
 #import <DConnectSDK/DConnectEvent.h>
