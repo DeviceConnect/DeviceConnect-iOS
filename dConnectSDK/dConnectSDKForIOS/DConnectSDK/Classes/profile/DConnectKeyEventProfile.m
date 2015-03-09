aae426231ded02cd1070e3797cecb0dc1d306be3
畠山剛雄 <takeo@gclue.jp>
2015-03-09 16:15:09 +0900

    Add KeyEvent Profile to SDK.

---
+++ dConnectSDK/dConnectSDKForIOS/DConnectSDK/Classes/profile/DConnectKeyEventProfile.m
@@ -0,0 +1,170 @@
+//
+//  DConnectKeyEventProfile.m
+//  DConnectSDK
+//
+//  Copyright (c) 2015 NTT DOCOMO,INC.
+//  Released under the MIT license
+//  http://opensource.org/licenses/mit-license.php
+//
+
+#import "DConnectKeyEventProfile.h"
+
+NSString *const DConnectKeyEventProfileName = @"button";
+NSString *const DConnectKeyEventProfileAttrKeyEvent = @"keyeven";
+NSString *const DConnectKeyEventProfileParamKeyEvent = @"keyevent";
+NSString *const DConnectKeyEventProfileParamId = @"id";
+NSString *const DConnectKeyEventProfileParamConfig = @"config";
+
+@interface DConnectKeyEventProfile()
+
+- (BOOL) hasMethod:(SEL)method response:(DConnectResponseMessage *)response;
+
+@end
+
+@implementation DConnectKeyEventProfile
+
+- (NSString *) profileName {
+    return DConnectKeyEventProfileName;
+}
+
+- (BOOL) didReceiveGetRequest:(DConnectRequestMessage *)request response:(DConnectResponseMessage *)response {
+    
+    BOOL send = YES;
+    
+    if (!_delegate) {
+        [response setErrorToNotSupportAction];
+        return send;
+    }
+    
+    NSString *attribute = [request attribute];
+    
+    if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnDown]) {
+        if ([self hasMethod:@selector(profile:didReceiveGetOnDownRequest:response:serviceId:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            send = [_delegate profile:self didReceiveGetOnDownRequest:request
+                             response:response serviceId:serviceId];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnUp]) {
+        if ([self hasMethod:@selector(profile:didReceiveGetOnUpRequest:response:serviceId:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            send = [_delegate profile:self didReceiveGetOnUpRequest:request
+                             response:response serviceId:serviceId];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else {
+        [response setErrorToUnknownAttribute];
+    }
+    
+    return send;
+}
+
+- (BOOL) didReceivePutRequest:(DConnectRequestMessage *)request response:(DConnectResponseMessage *)response {
+    
+    BOOL send = YES;
+    
+    if (!_delegate) {
+        [response setErrorToNotSupportAction];
+        return send;
+    }
+    
+    NSString *attribute = [request attribute];
+    
+    if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnDown]) {
+        if ([self hasMethod:@selector(profile:didReceivePutOnDownRequest:response:serviceId:sessionKey:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            NSString *sessionKey = [request sessionKey];
+            send = [_delegate profile:self didReceivePutOnDownRequest:request
+                             response:response serviceId:serviceId sessionKey:sessionKey];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnUp]) {
+        if ([self hasMethod:@selector(profile:didReceivePutOnUpRequest:response:serviceId:sessionKey:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            NSString *sessionKey = [request sessionKey];
+            send = [_delegate profile:self didReceivePutOnUpRequest:request
+                             response:response serviceId:serviceId sessionKey:sessionKey];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else {
+        [response setErrorToUnknownAttribute];
+    }
+    
+    return send;
+}
+
+- (BOOL) didReceiveDeleteRequest:(DConnectRequestMessage *)request response:(DConnectResponseMessage *)response {
+    
+    BOOL send = YES;
+    
+    if (!_delegate) {
+        [response setErrorToNotSupportAction];
+        return send;
+    }
+    
+    NSString *attribute = [request attribute];
+    
+    if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnDown]) {
+        if ([self hasMethod:@selector(profile:didReceiveDeleteOnDownRequest:response:serviceId:sessionKey:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            NSString *sessionKey = [request sessionKey];
+            send = [_delegate profile:self didReceiveDeleteOnDownRequest:request response:response
+                             serviceId:serviceId sessionKey:sessionKey];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else if ([attribute isEqualToString:DConnectKeyEventProfileAttrOnUp]) {
+        if ([self hasMethod:@selector(profile:didReceiveDeleteOnUpRequest:response:serviceId:sessionKey:)
+                   response:response])
+        {
+            NSString *serviceId = [request serviceId];
+            NSString *sessionKey = [request sessionKey];
+            send = [_delegate profile:self didReceiveDeleteOnUpRequest:request response:response
+                             serviceId:serviceId sessionKey:sessionKey];
+        } else {
+            [response setErrorToUnknownAttribute];
+        }
+    } else {
+        [response setErrorToUnknownAttribute];
+    }
+    
+    return send;
+}
+
+#pragma mark - Setter
++ (void) setKeyEvent:(DConnectMessage *)keyevent target:(DConnectMessage *)message {
+    [message setMessage:keyevent forKey:DConnectKeyEventProfileParamKeyEvent];
+}
+
++ (void) setId:(int)id target:(DConnectMessage *)message {
+    [message setInteger:id forKey:DConnectKeyEventProfileParamId];
+}
+
++ (void) setConfig:(NSString *)config target:(DConnectMessage *)message {
+    [message setString:config forKey:DConnectKeyEventProfileParamConfig];
+}
+
+#pragma mark - Private Methods
+- (BOOL) hasMethod:(SEL)method response:(DConnectResponseMessage *)response {
+    BOOL result = [_delegate respondsToSelector:method];
+    if (!result) {
+        [response setErrorToNotSupportAttribute];
+    }
+    return result;
+}
+
+@end
