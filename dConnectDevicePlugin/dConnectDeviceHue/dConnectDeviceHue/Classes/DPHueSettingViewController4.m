97e0d57ea2c03f4e84428d12a525b86dd46d010e
TakayukiHoshi1984 <hoshi@gclue.jp>
2015-05-28 13:49:28 +0900

    Support iOS7

--- dConnectDevicePlugin/dConnectDeviceHue/dConnectDeviceHue/Classes/DPHueSettingViewController4.m
+++ dConnectDevicePlugin/dConnectDeviceHue/dConnectDeviceHue/Classes/DPHueSettingViewController4.m
@@ -70,7 +70,7 @@
     cell.exclusiveTouch = YES;
     cell.accessoryView.exclusiveTouch = YES;
     NSString * path = [_bundle pathForResource:@"hue_small_icon" ofType:@"png"];
-    cell.imageView.image = [UIImage imageNamed:path];
+    cell.imageView.image = [UIImage imageWithContentsOfFile:path];
     NSDictionary* dic = [[DPHueManager sharedManager] getLightStatus];
     int i = 0;
     for (PHLight *light in dic.allValues) {
@@ -110,32 +110,65 @@
 }
 
 - (IBAction)searchManual:(id)sender {
-    UIAlertController *serialAlert = [UIAlertController alertControllerWithTitle:DPHueLocalizedString(_bundle, @"HueSerialNoTitle")
-                                                                         message:DPHueLocalizedString(_bundle, @"HueSerialNoDesc")
-                                                                  preferredStyle:UIAlertControllerStyleAlert];
+    float osVersion = [[[UIDevice currentDevice] systemVersion] floatValue];
+    if (osVersion > 8.0) {
+        UIAlertController *serialAlert = [UIAlertController alertControllerWithTitle:DPHueLocalizedString(_bundle, @"HueSerialNoTitle")
+                                                                             message:DPHueLocalizedString(_bundle, @"HueSerialNoDesc")
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDestructive handler:nil];
+        [serialAlert addAction:cancelAction];
+        _okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            _indicator.hidden = NO;
+            [self startIndicator];
+            NSArray *serials = @[_serial];
+            [manager registerLightForSerialNo:serials completion:^(NSArray *errors) {
+                retryCount = 2;
+                lightCount = (int) [[DPHueManager sharedManager] getLightStatus].allValues.count;
+                [self  reloadHue];
+            }];
+        }];
+        _okAction.enabled = NO;
+        [serialAlert addAction:_okAction];
+
+        [serialAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+            
+            textField.placeholder = DPHueLocalizedString(_bundle, @"HueSerialNoHint");
+            textField.delegate = self;
+            textField.keyboardType = UIKeyboardTypeAlphabet;
+        }];
+        [self presentViewController:serialAlert animated:YES completion:nil];
+    } else {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:DPHueLocalizedString(_bundle, @"HueSerialNoTitle")
+                                                        message:DPHueLocalizedString(_bundle, @"HueSerialNoDesc")
+                                                       delegate:self
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:@"OK", nil];
+        alert.delegate       = self;
+        alert.alertViewStyle = UIAlertViewStyleSecureTextInput;
+        [alert textFieldAtIndex:0].placeholder = DPHueLocalizedString(_bundle, @"HueSerialNoHint");
+        [alert textFieldAtIndex:0].delegate = self;
+        [alert textFieldAtIndex:0].keyboardType = UIKeyboardTypeAlphabet;
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if( buttonIndex == alertView.cancelButtonIndex ) { return; }
     
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDestructive handler:nil];
-    [serialAlert addAction:cancelAction];
-    _okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+    NSString* textValue = [[alertView textFieldAtIndex:0] text];
+    if( [textValue length] >= 6 )
+    {
         _indicator.hidden = NO;
         [self startIndicator];
-        NSArray *serials = @[_serial];
+        NSArray *serials = @[textValue];
         [manager registerLightForSerialNo:serials completion:^(NSArray *errors) {
             retryCount = 2;
             lightCount = (int) [[DPHueManager sharedManager] getLightStatus].allValues.count;
             [self  reloadHue];
         }];
-    }];
-    _okAction.enabled = NO;
-    [serialAlert addAction:_okAction];
-    
-    [serialAlert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        
-        textField.placeholder = DPHueLocalizedString(_bundle, @"HueSerialNoHint");
-        textField.delegate = self;
-        textField.keyboardType = UIKeyboardTypeAlphabet;
-    }];
-    [self presentViewController:serialAlert animated:YES completion:nil];
+    }
 }
 
 
