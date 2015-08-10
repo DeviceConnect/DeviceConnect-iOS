ebfc3445974e193c4e94723b625fbc9e0df6baf3
Shigekazu Fukui <fukui@gclue.jp>
2015-08-10 10:12:06 +0900

    Fix tap action of App Store logo: it now properly opens a URL.

--- dConnectDevicePlugin/dConnectDeviceAllJoyn/dConnectDeviceAllJoyn/Sources/Views/Tutorials/DPAllJoynSettingDetailLIFXViewController.m
+++ dConnectDevicePlugin/dConnectDeviceAllJoyn/dConnectDeviceAllJoyn/Sources/Views/Tutorials/DPAllJoynSettingDetailLIFXViewController.m
@@ -41,9 +41,15 @@
 
 - (void)didImageTapped
 {
-    [[UIApplication sharedApplication]
-     openURL:[NSURL URLWithString:
-              @"itms-apps://geo.itunes.apple.com/app/lifx/id657758311?mt=8"]];
+    UIApplication *app = [UIApplication sharedApplication];
+    NSURL *url =
+    [NSURL URLWithString:
+     @"itms-apps://itunes.apple.com/app/lifx/id657758311?mt=8"];
+    if ([app canOpenURL:url]) {
+        [app openURL:url];
+    } else {
+        DCLogError(@"Could not open App Store URL.");
+    }
 }
 
 @end
