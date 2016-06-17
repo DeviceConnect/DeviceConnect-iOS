56ec2a61960fbc0af6d0f9f24fad9177249a8f01
Tetsuya Hirano <hirano@hirano-dept.com>
2016-06-17 15:00:12 +0900

    Add updateDatasource function

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.h
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.h
@@ -13,9 +13,11 @@
 @property (strong, nonatomic) NSMutableArray* datasource;
 @property (nonatomic, strong) GHURLManager *manager;
 @property (nonatomic) NSString* url;
+@property (nonatomic, readonly) BOOL isBookmarksEmpty;
+@property (nonatomic, readonly) BOOL isDeviceEmpty;
 
 - (void)initialSetup;
-- (void)finishOriginBlock;
+- (void)saveOriginBlock;
 - (NSString*)checkUrlString:(NSString*)url;
 - (NSString*)makeURLFromNotification:(NSNotification*)notif;
 
--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.m
@@ -8,19 +8,91 @@
 
 #import "TopViewModel.h"
 #import <DConnectSDK/DConnectSDK.h>
+#import "GHDataManager.h"
 
 @implementation TopViewModel
 
+//--------------------------------------------------------------//
+#pragma mark - 初期化
+//--------------------------------------------------------------//
 - (instancetype)init
 {
     self = [super init];
     if (self) {
         self.manager = [[GHURLManager alloc]init];
         self.url = @"http://www.google.com";
+        self.datasource = [[NSMutableArray alloc]initWithObjects:
+                           [[NSArray alloc]init],
+                           [[NSArray alloc]init],
+                           nil];
+        [self updateDatasource];
     }
     return self;
 }
 
+- (void)dealloc
+{
+    self.manager = nil;
+    self.url = nil;
+    self.datasource = nil;
+}
+
+//--------------------------------------------------------------//
+#pragma mark - datasource生成
+//--------------------------------------------------------------//
+- (void)updateDatasource
+{
+    [self.datasource replaceObjectAtIndex:0 withObject:[self setupBookmarks]];
+    [self.datasource replaceObjectAtIndex:1 withObject:[self setupDevices]];
+}
+
+
+//--------------------------------------------------------------//
+#pragma mark - bookmarks
+//--------------------------------------------------------------//
+static NSInteger maxIconCount = 8;
+- (NSArray*)setupBookmarks
+{
+    NSMutableArray* bookmarks = [[self fetchBookmarks]mutableCopy];
+    _isBookmarksEmpty = (bookmarks == nil);
+    if (_isBookmarksEmpty) {
+        bookmarks = [[NSMutableArray alloc]init];
+    }
+    //maxIconCountに達していない場合はダミーを作成する
+    while (bookmarks.count <= maxIconCount) {
+        GHPageModel* page = [[GHPageModel alloc]init];
+        page.type = TYPE_BOOKMARK_DUMMY;
+        [bookmarks addObject:page];
+    }
+    return bookmarks;
+}
+
+- (NSArray*)fetchBookmarks
+{
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"latest_opened_date != NULL"];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"latest_opened_date" ascending:NO];
+    NSArray* bookmarks = [[GHDataManager shareManager]getModelDataByPredicate:predicate
+                                                          withSortDescriptors:@[sortDescriptor]
+                                                                   entityName:@"Page"
+                                                                        limit: maxIconCount
+                                                                      context:nil];
+    return bookmarks;
+}
+
+
+
+//--------------------------------------------------------------//
+#pragma mark - Devices
+//--------------------------------------------------------------//
+- (NSArray*)setupDevices
+{
+    return [[NSArray alloc]init];
+}
+
+
+//--------------------------------------------------------------//
+#pragma mark - useOriginBlocking 更新
+//--------------------------------------------------------------//
 - (void)initialSetup
 {
     DConnectManager *mgr = [DConnectManager sharedManager];
@@ -28,12 +100,16 @@
     mgr.settings.useOriginBlocking = isOriginBlock;
 }
 
-- (void)finishOriginBlock
+- (void)saveOriginBlock
 {
     DConnectManager *mgr = [DConnectManager sharedManager];
     [[NSUserDefaults standardUserDefaults] setBool:mgr.settings.useOriginBlocking forKey:IS_ORIGIN_BLOCKING];
 }
 
+
+//--------------------------------------------------------------//
+#pragma mark - URL
+//--------------------------------------------------------------//
 - (NSString*)checkUrlString:(NSString*)url
 {
     //文字列がURLの場合
