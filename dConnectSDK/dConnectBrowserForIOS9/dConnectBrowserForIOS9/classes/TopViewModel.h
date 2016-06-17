8d6c911aa5e0f6dfd978ab363eca6e6ff0ae34b4
Tetsuya Hirano <hirano@hirano-dept.com>
2016-06-17 11:48:47 +0900

    Add TopViewModel

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.h
@@ -0,0 +1,13 @@
+//
+//  TopViewModel.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/17.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TopViewModel : NSObject
+
+@end
---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/TopViewModel.m
@@ -0,0 +1,13 @@
+//
+//  TopViewModel.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/17.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "TopViewModel.h"
+
+@implementation TopViewModel
+
+@end
