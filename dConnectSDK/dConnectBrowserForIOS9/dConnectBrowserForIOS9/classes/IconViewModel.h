1a2cea49077c3cad6822578e86f939cb6534ec8c
Tetsuya Hirano <hirano@hirano-dept.com>
2016-06-17 10:31:07 +0900

    Add IconViewModel

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/IconViewModel.h
@@ -0,0 +1,13 @@
+//
+//  IconViewModel.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/17.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IconViewModel : NSObject
+
+@end
