871c6497e631a0e6bbc98dff8fa8820797fc46fe
Tetsuya Hirano <hirano@hirano-dept.com>
2016-06-23 14:51:39 +0900

    Add 3 type of cells

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DetailableCell.h
@@ -0,0 +1,13 @@
+//
+//  DetailableCell.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/23.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailableCell : UITableViewCell
+
+@end
---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GrayLabelCell.h
@@ -0,0 +1,13 @@
+//
+//  GrayLabelCell.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/23.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GrayLabelCell : UITableViewCell
+
+@end
