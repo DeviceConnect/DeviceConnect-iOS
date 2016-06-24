300a6837946bf42d27b6dac3a40610e189f3ddbb
Tetsuya Hirano <hirano@hirano-dept.com>
2016-06-24 17:37:47 +0900

    WebViewControllerを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/06/24.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
