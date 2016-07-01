5322706d45395f7c511e1c1321a30ef7f355050b
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-01 14:54:20 +0900

    DeviceIconViewModelを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewModel.h
@@ -0,0 +1,13 @@
+//
+//  DeviceIconViewModel.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceIconViewModel : NSObject
+
+@end
---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewModel.m
@@ -0,0 +1,13 @@
+//
+//  DeviceIconViewModel.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "DeviceIconViewModel.h"
+
+@implementation DeviceIconViewModel
+
+@end
