8b438684c55ace866e43ef4b4010b14ec67b0950
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-01 19:58:28 +0900

    GHDeviceListViewControllerを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDeviceListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GHDeviceListViewController.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHDeviceListViewController : UIViewController
+
+@end
---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDeviceListViewController.m
@@ -0,0 +1,22 @@
+//
+//  GHDeviceListViewController.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "GHDeviceListViewController.h"
+
+@interface GHDeviceListViewController ()
+
+@end
+
+@implementation GHDeviceListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"デバイス一覧";
+}
+
+@end
