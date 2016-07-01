19598ddba30402b33911aba7a8ef65fbb5cd91d0
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-01 10:02:34 +0900

    GHDeviceUtilクラスを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDeviceUtil.h
@@ -0,0 +1,13 @@
+//
+//  GHDeviceUtil.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GHDeviceUtil : NSObject
+
+@end
---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDeviceUtil.m
@@ -0,0 +1,13 @@
+//
+//  GHDeviceUtil.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/01.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "GHDeviceUtil.h"
+
+@implementation GHDeviceUtil
+
+@end
