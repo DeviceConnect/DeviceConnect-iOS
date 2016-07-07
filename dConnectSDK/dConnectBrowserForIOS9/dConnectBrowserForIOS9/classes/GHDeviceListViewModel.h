8b46203bfb17b29bd1828b06f996ff28c5429b9b
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-07 11:14:20 +0900

    GHDeviceListViewModelを新規追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDeviceListViewModel.h
@@ -0,0 +1,13 @@
+//
+//  GHDeviceListViewModel.h
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/07.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GHDeviceListViewModel : NSObject
+@property (strong, nonatomic) NSMutableArray* datasource;
+@end
