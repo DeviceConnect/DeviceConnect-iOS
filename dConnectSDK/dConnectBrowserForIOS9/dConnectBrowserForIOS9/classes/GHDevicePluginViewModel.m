f56f0b299bb8ff3c68d9ecb13b793373fe24c181
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-07 16:54:55 +0900

    GHDevicePluginViewModelを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDevicePluginViewModel.m
@@ -0,0 +1,13 @@
+//
+//  GHDevicePluginViewModel.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/07.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "GHDevicePluginViewModel.h"
+
+@implementation GHDevicePluginViewModel
+
+@end
