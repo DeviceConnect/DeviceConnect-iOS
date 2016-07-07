fef49f2131f5115023c2430922c179ac6c37c216
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-07 16:54:00 +0900

    GHDevicePluginTableViewControllerを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDevicePluginTableViewController.m
@@ -0,0 +1,27 @@
+//
+//  GHDevicePluginTableViewController.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/07.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "GHDevicePluginTableViewController.h"
+
+@interface GHDevicePluginTableViewController ()
+
+@end
+
+@implementation GHDevicePluginTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+
+@end
