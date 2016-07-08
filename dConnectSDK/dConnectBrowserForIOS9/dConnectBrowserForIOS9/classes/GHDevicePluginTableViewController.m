e2f1d7a61805fe8a6c940c9d777d9ff5203cb0c1
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-08 11:58:46 +0900

    GHDevicePluginTableViewControllerを編集

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDevicePluginTableViewController.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDevicePluginTableViewController.m
@@ -7,21 +7,67 @@
 //
 
 #import "GHDevicePluginTableViewController.h"
+#import "GHDevicePluginViewModel.h"
+#import "GHDevicePluginViewCell.h"
 
 @interface GHDevicePluginTableViewController ()
-
+{
+    GHDevicePluginViewModel* viewModel;
+}
 @end
 
 @implementation GHDevicePluginTableViewController
 
++ (GHDevicePluginTableViewController*)instantiate
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"DevicePlugin" bundle:[NSBundle mainBundle]];
+    return (GHDevicePluginTableViewController*)[storyboard instantiateViewControllerWithIdentifier:@"GHDevicePluginTableViewController"];
+}
+
+//--------------------------------------------------------------//
+#pragma mark - button
+//--------------------------------------------------------------//
+- (void)close:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+//--------------------------------------------------------------//
+#pragma mark - view cycle
+//--------------------------------------------------------------//
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    UIBarButtonItem *closeButton = [[UIBarButtonItem alloc]initWithTitle:@"閉じる"
+                                                                   style:UIBarButtonItemStylePlain
+                                                                  target:self
+                                                                  action:@selector(close:)];
+    self.navigationItem.leftBarButtonItem = closeButton;
+    self.title = @"デバイスプラグイン";
+    viewModel = [[GHDevicePluginViewModel alloc]init];
+    self.tableView.estimatedRowHeight = 80;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
-#pragma mark - Table view data source
+//--------------------------------------------------------------//
+#pragma mark - tableViewDelegate
+//--------------------------------------------------------------//
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return [viewModel.datasource count];
 }
 
+- (GHDevicePluginViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    DConnectDevicePlugin* plugin = [viewModel.datasource objectAtIndex:indexPath.row];
+    GHDevicePluginViewCell *cell = (GHDevicePluginViewCell*)[tableView dequeueReusableCellWithIdentifier:@"GHDevicePluginViewCell" forIndexPath:indexPath];
+    [cell configureCell:plugin];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    //TODO: デバイスプラグイン設定画面
+}
 
 @end
