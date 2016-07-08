0a4f921da0248d3e0371cd700e1793d8641eb802
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-08 18:18:28 +0900

    GHDevicePluginDetailViewControllerを追加

---
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHDevicePluginDetailViewController.m
@@ -0,0 +1,21 @@
+//
+//  GHDevicePluginDetailViewController.m
+//  dConnectBrowserForIOS9
+//
+//  Created by Tetsuya Hirano on 2016/07/08.
+//  Copyright © 2016年 GClue,Inc. All rights reserved.
+//
+
+#import "GHDevicePluginDetailViewController.h"
+
+@interface GHDevicePluginDetailViewController ()
+
+@end
+
+@implementation GHDevicePluginDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
