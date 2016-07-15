90bd8bbecc67a7442f268a553d7d3f1da696f772
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-15 11:27:11 +0900

    configureCellメソッドをGHBookmarkCellへ移動

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHBookmarkCell.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/GHBookmarkCell.m
@@ -11,26 +11,41 @@
 
 @implementation GHBookmarkCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+/**
+ * セルの表示内容をセット
+ * @param page Pageモデル
+ * @param indexPath indexPath
+ * @param controller NSFetchedResultsController
+ */
+- (void)configureCell:(Page *)page atIndexPath:(NSIndexPath *)indexPath isEditing:(BOOL)isEditing
 {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+    self.accessoryType = UITableViewCellAccessoryNone;
+    self.editingAccessoryType = UITableViewCellAccessoryNone;
+    self.myPage = page;
+
+    self.textLabel.text = page.title;
+
+    if ([page.type isEqualToString:TYPE_FAVORITE]) {
+        //お気に入り
+        self.imageView.image = [UIImage imageNamed:@"star"];
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+        if (isEditing) {
+            self.textLabel.tintColor = [UIColor grayColor];
+        }
+
+    }else if ([page.type isEqualToString:TYPE_BOOKMARK]){
+        //ブックマーク
+        self.imageView.image = [UIImage imageNamed:@"bookmark"];
+
+        //編集中のアクセサリー
+        self.editingAccessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+    }else if ([page.type isEqualToString:TYPE_FOLDER]){
+        //フォルダ
+        self.imageView.image = [UIImage imageNamed:@"folder"];
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
-    return self;
 }
-
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 
 @end
