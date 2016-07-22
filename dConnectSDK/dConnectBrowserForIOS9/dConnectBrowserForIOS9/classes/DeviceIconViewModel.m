dcb564fc7794899475f9e1da382856cdc50c3000
Tetsuya Hirano <hirano@hirano-dept.com>
2016-07-22 14:03:14 +0900

    使用するアイコン画像の選択とタイプの画像ファイル名を作成する処理を追加

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewModel.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewModel.m
@@ -17,7 +17,7 @@
 
 - (BOOL)isOnline
 {
-    return (BOOL)[self.message arrayForKey: DConnectServiceDiscoveryProfileParamOnline];
+    return [(NSNumber*)[self.message arrayForKey: DConnectServiceDiscoveryProfileParamOnline] boolValue];
 }
 
 - (NSString*)idName
@@ -29,6 +29,35 @@
 {
     return (NSString*)[self.message arrayForKey: DConnectServiceDiscoveryProfileParamType];
 }
+
+- (UIImage*)iconImage
+{
+    NSString* target = @"dConnectDevicePebble"; //TODO: DConnectMessage.idからターゲットを判定する
+    NSString* bundle = [[NSBundle mainBundle] pathForResource:target ofType:@"bundle"];
+    if (bundle) {
+        NSString* imagePath = [NSString stringWithFormat:@"%@/dconnect_icon.png", bundle];
+        return [[UIImage alloc] initWithContentsOfFile:imagePath];
+    } else {
+        return [UIImage imageNamed:@"default_device_icon"];
+    }
+}
+
+- (NSString*)typeIconFilename
+{
+    NSString* type = (NSString*)[self.message arrayForKey: @"type"];
+    if ([type isEqualToString:DConnectServiceDiscoveryProfileNetworkTypeWiFi]) {
+        return self.isOnline ? @"wifi_on" : @"wifi_off";
+    } else if ([type isEqualToString:DConnectServiceDiscoveryProfileNetworkTypeBluetooth] ||
+               [type isEqualToString:DConnectServiceDiscoveryProfileNetworkTypeBLE]) {
+        return self.isOnline ? @"bluetooth_on" : @"bluetooth_off";
+    } else if ([type isEqualToString:DConnectServiceDiscoveryProfileNetworkTypeNFC]) {
+        return self.isOnline ? @"nfc_on" : @"nfc_off";
+    } else {
+        //NOTE: Unknown or type not exist
+        return nil;
+    }
+}
+
 
 - (void)dealloc
 {
