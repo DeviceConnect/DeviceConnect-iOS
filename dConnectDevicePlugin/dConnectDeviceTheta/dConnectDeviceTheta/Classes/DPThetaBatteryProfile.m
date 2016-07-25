b02983603f6dad899d3f49e791ba5388e0506f24
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2016-07-25 15:33:48 +0900

    ThetaデバイスプラグインのBatteryProfileを新構造へ変更。

--- dConnectDevicePlugin/dConnectDeviceTheta/dConnectDeviceTheta/Classes/DPThetaBatteryProfile.m
+++ dConnectDevicePlugin/dConnectDeviceTheta/dConnectDeviceTheta/Classes/DPThetaBatteryProfile.m
@@ -22,46 +22,46 @@
     self = [super init];
     if (self) {
         self.delegate = self;
+        
+        // API登録(didReceiveGetLevelRequest相当)
+        NSString *getLevelRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                      interfaceName: nil
+                                                      attributeName: DConnectBatteryProfileAttrLevel];
+        [self addGetPath: getLevelRequestApiPath api: ^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+            
+            CONNECT_CHECK();
+            float level = [[DPThetaManager sharedManager] getBatteryLevel] / (float) 100.0;
+            if (level < 0 || level > 1) {
+                // 未知のステータス；エラーレスポンスを返す。
+                [response setErrorToUnknownWithMessage:@"Battery status is unknown."];
+            } else {
+                [DConnectBatteryProfile setLevel:level target:response];
+                [response setResult:DConnectMessageResultTypeOk];
+            }
+            return YES;
+            
+        }];
+        
+        // API登録(didReceiveGetAllRequest相当)
+        NSString *getAllRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                    interfaceName: nil
+                                                    attributeName: nil];
+        [self addGetPath: getAllRequestApiPath api: ^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+            CONNECT_CHECK();
+            float level = [[DPThetaManager sharedManager] getBatteryLevel] / (float) 100.0;
+            if (level >= 0 && level <= 1) {
+                [response setResult:DConnectMessageResultTypeOk];
+                [DConnectBatteryProfile setCharging:NO target:response];
+                [DConnectBatteryProfile setLevel:level target:response];
+            } else {
+                // 未知のステータス；エラーレスポンスを返す。
+                [response setErrorToUnknownWithMessage:@"Battery status is unknown."];
+            }
+            return YES;
+        }];
+        
     }
     return self;
 }
-
-#pragma mark - Get Methods
-
-- (BOOL)        profile:(DConnectBatteryProfile *)profile
-didReceiveGetAllRequest:(DConnectRequestMessage *)request
-               response:(DConnectResponseMessage *)response
-               serviceId:(NSString *)serviceId
-{
-    CONNECT_CHECK();
-    float level = [[DPThetaManager sharedManager] getBatteryLevel] / (float) 100.0;
-    if (level >= 0 && level <= 1) {
-        [response setResult:DConnectMessageResultTypeOk];
-        [DConnectBatteryProfile setCharging:NO target:response];
-        [DConnectBatteryProfile setLevel:level target:response];
-    } else {
-        // 未知のステータス；エラーレスポンスを返す。
-        [response setErrorToUnknownWithMessage:@"Battery status is unknown."];
-    }
-    return YES;
-}
-
-- (BOOL)          profile:(DConnectBatteryProfile *)profile
-didReceiveGetLevelRequest:(DConnectRequestMessage *)request
-                 response:(DConnectResponseMessage *)response
-                 serviceId:(NSString *)serviceId
-{
-    CONNECT_CHECK();
-    float level = [[DPThetaManager sharedManager] getBatteryLevel] / (float) 100.0;
-    if (level < 0 || level > 1) {
-        // 未知のステータス；エラーレスポンスを返す。
-        [response setErrorToUnknownWithMessage:@"Battery status is unknown."];
-    } else {
-        [DConnectBatteryProfile setLevel:level target:response];
-        [response setResult:DConnectMessageResultTypeOk];
-    }
-    return YES;
-}
-
 
 @end
