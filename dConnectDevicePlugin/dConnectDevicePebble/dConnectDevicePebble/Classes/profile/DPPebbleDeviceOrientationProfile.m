91edf9de7833a64df72a7bc1efaf8e8d77b1c9a8
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2016-07-26 18:39:42 +0900

    PebbleデバイスプラグインのDeviceOrientationProfileを新構造へ変更。

--- dConnectDevicePlugin/dConnectDevicePebble/dConnectDevicePebble/Classes/profile/DPPebbleDeviceOrientationProfile.m
+++ dConnectDevicePlugin/dConnectDevicePebble/dConnectDevicePebble/Classes/profile/DPPebbleDeviceOrientationProfile.m
@@ -46,6 +46,118 @@
 	if (self) {
 		self.delegate = self;
         self.cacheOrientationData = nil;
+        __weak DPPebbleDeviceOrientationProfile *weakSelf = self;
+        
+        // API登録(didReceiveGetOnDeviceOrientationRequest相当)
+        NSString *getOnDeviceOrientationRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                                    interfaceName: nil
+                                                                    attributeName: DConnectDeviceOrientationProfileAttrOnDeviceOrientation];
+        [self addGetPath: getOnDeviceOrientationRequestApiPath
+                     api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                         NSString *serviceId = [request serviceId];
+                         if ([weakSelf hasEventList:serviceId]) {
+                             if ([weakSelf cacheOrientationData]) {
+                                 [response setResult:DConnectMessageResultTypeOk];
+                                 [DConnectDeviceOrientationProfile setOrientation:[weakSelf cacheOrientationData] target:response];
+                             } else {
+                                 [response setErrorToIllegalDeviceStateWithMessage:@"device is not ready."];
+                             }
+                             return YES;
+                         }
+                         
+                         [[DPPebbleManager sharedManager] registDeviceOrientationEvent:serviceId callback:^(NSError *error) {
+                             if (error) {
+                                 [response setErrorToIllegalDeviceStateWithMessage:@"device is not ready."];
+                                 [[DConnectManager sharedManager] sendResponse:response];
+                             }
+                         } eventCallback:^(float accelX, float accelY, float accelZ, long long interval) {
+                             DConnectMessage * orientation = [weakSelf createOrientationWithAccelX:accelX
+                                                                                            accelY:accelY
+                                                                                            accelZ:accelZ
+                                                                                          interval:interval];
+                             [response setResult:DConnectMessageResultTypeOk];
+                             [DConnectDeviceOrientationProfile setOrientation:orientation target:response];
+                             
+                             [[DConnectManager sharedManager] sendResponse:response];
+                             
+                             if (![weakSelf hasEventList:serviceId]) {
+                                 [[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
+                                     if (error) {
+                                         NSLog(@"Error:%@", error);
+                                     }
+                                 }];
+                             }
+                         }];
+                         return NO;
+                     }];
+        
+        // API登録(didReceivePutOnDeviceOrientationRequest相当)
+        NSString *putOnDeviceOrientationRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                                    interfaceName: nil
+                                                                    attributeName: DConnectDeviceOrientationProfileAttrOnDeviceOrientation];
+        [self addPutPath: putOnDeviceOrientationRequestApiPath
+                     api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                         NSString *serviceId = [request serviceId];
+                         __block BOOL responseFlg = YES;
+                         // イベント登録
+                         [DPPebbleProfileUtil handleRequest:request response:response isRemove:NO callback:^{
+                             
+                             // Pebbleに登録
+                             [[DPPebbleManager sharedManager] registDeviceOrientationEvent:serviceId callback:^(NSError *error) {
+                                 // 登録成功
+                                 // エラーチェック
+                                 [DPPebbleProfileUtil handleErrorNormal:error response:response];
+                             } eventCallback:^(float accelX, float accelY, float accelZ, long long interval) {
+                                 // イベントコールバック
+                                 // DConnectメッセージ作成
+                                 DConnectMessage * message = [weakSelf createOrientationWithAccelX:accelX
+                                                                                            accelY:accelY
+                                                                                            accelZ:accelZ
+                                                                                          interval:interval];
+                                 // キャッシュ保持
+                                 weakSelf.cacheOrientationData = message;
+                                 // DConnectにイベント送信
+                                 [DPPebbleProfileUtil sendMessageWithProvider:weakSelf.provider
+                                                                      profile:DConnectDeviceOrientationProfileName
+                                                                    attribute:DConnectDeviceOrientationProfileAttrOnDeviceOrientation
+                                                                    serviceID:serviceId
+                                                              messageCallback:^(DConnectMessage *eventMsg) {
+                                                                  // イベントにメッセージ追加
+                                                                  [DConnectDeviceOrientationProfile setOrientation:message target:eventMsg];
+                                                              } deleteCallback:^ {
+                                                                  // Pebbleのイベント削除
+                                                                  [[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
+                                                                      if (error) {
+                                                                          NSLog(@"Error:%@", error);
+                                                                      }
+                                                                  }];
+                                                              }];
+                             }];
+                             
+                             responseFlg = NO;
+                         }];
+                         
+                         return responseFlg;
+                     }];
+        
+        // API登録(didReceiveDeleteOnDeviceOrientationRequest相当)
+        NSString *deleteOnDeviceOrientationRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                                       interfaceName: nil
+                                                                       attributeName: DConnectDeviceOrientationProfileAttrOnDeviceOrientation];
+        [self addDeletePath: deleteOnDeviceOrientationRequestApiPath
+                        api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                            NSString *serviceId = [request serviceId];
+                            // DConnectイベント削除
+                            [DPPebbleProfileUtil handleRequest:request response:response isRemove:YES callback:^{
+                                // Pebbleのイベント削除
+                                [[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
+                                    if (error) {
+                                        NSLog(@"Error:%@", error);
+                                    }
+                                }];
+                            }];
+                            return YES;
+                        }];
     }
 	return self;
 }
@@ -71,119 +183,6 @@
     [DConnectDeviceOrientationProfile setAccelerationIncludingGravity:accelerationIncludingGravity target:message];
     [DConnectDeviceOrientationProfile setInterval:interval target:message];
     return message;
-}
-
-#pragma mark - DConnectDeviceOrientationProfileDelegate
-
-- (BOOL)                        profile:(DConnectDeviceOrientationProfile *)profile
-didReceiveGetOnDeviceOrientationRequest:(DConnectRequestMessage *)request
-                               response:(DConnectResponseMessage *)response
-                              serviceId:(NSString *)serviceId
-{
-    if ([self hasEventList:serviceId]) {
-        if (self.cacheOrientationData) {
-            [response setResult:DConnectMessageResultTypeOk];
-            [DConnectDeviceOrientationProfile setOrientation:self.cacheOrientationData target:response];
-        } else {
-            [response setErrorToIllegalDeviceStateWithMessage:@"device is not ready."];
-        }
-        return YES;
-    }
-    __unsafe_unretained typeof(self) weakSelf = self;
-
-    [[DPPebbleManager sharedManager] registDeviceOrientationEvent:serviceId callback:^(NSError *error) {
-        if (error) {
-            [response setErrorToIllegalDeviceStateWithMessage:@"device is not ready."];
-            [[DConnectManager sharedManager] sendResponse:response];
-        }
-    } eventCallback:^(float accelX, float accelY, float accelZ, long long interval) {
-        DConnectMessage * orientation = [self createOrientationWithAccelX:accelX
-                                                                   accelY:accelY
-                                                                   accelZ:accelZ
-                                                                 interval:interval];
-        [response setResult:DConnectMessageResultTypeOk];
-        [DConnectDeviceOrientationProfile setOrientation:orientation target:response];
-        
-        [[DConnectManager sharedManager] sendResponse:response];
-        
-        if (![weakSelf hasEventList:serviceId]) {
-            [[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
-                if (error) {
-                    NSLog(@"Error:%@", error);
-                }
-            }];
-        }
-    }];
-    return NO;
-}
-
-// ondeviceorientationイベント登録リクエストを受け取った
-- (BOOL)                        profile:(DConnectDeviceOrientationProfile *)profile
-didReceivePutOnDeviceOrientationRequest:(DConnectRequestMessage *)request
-                               response:(DConnectResponseMessage *)response
-                               serviceId:(NSString *)serviceId
-                             sessionKey:(NSString *)sessionKey
-{
-    __unsafe_unretained typeof(self) weakSelf = self;
-	__block BOOL responseFlg = YES;
-	// イベント登録
-	[DPPebbleProfileUtil handleRequest:request response:response isRemove:NO callback:^{
-		
-		// Pebbleに登録
-		[[DPPebbleManager sharedManager] registDeviceOrientationEvent:serviceId callback:^(NSError *error) {
-			// 登録成功
-			// エラーチェック
-			[DPPebbleProfileUtil handleErrorNormal:error response:response];
-		} eventCallback:^(float accelX, float accelY, float accelZ, long long interval) {
-			// イベントコールバック
-			// DConnectメッセージ作成
-            DConnectMessage * message = [weakSelf createOrientationWithAccelX:accelX
-                                                                   accelY:accelY
-                                                                   accelZ:accelZ
-                                                                 interval:interval];
-            // キャッシュ保持
-            weakSelf.cacheOrientationData = message;
-			// DConnectにイベント送信
-			[DPPebbleProfileUtil sendMessageWithProvider:weakSelf.provider
-												 profile:DConnectDeviceOrientationProfileName
-											   attribute:DConnectDeviceOrientationProfileAttrOnDeviceOrientation
-												serviceID:serviceId
-										 messageCallback:^(DConnectMessage *eventMsg) {
-				 // イベントにメッセージ追加
-				 [DConnectDeviceOrientationProfile setOrientation:message target:eventMsg];
-			 } deleteCallback:^ {
-				 // Pebbleのイベント削除
-				 [[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
-                     if (error) {
-                         NSLog(@"Error:%@", error);
-                     }
-				 }];
-			 }];
-		}];
-		
-		responseFlg = NO;
-	}];
-	
-	return responseFlg;
-}
-
-// ondeviceorientationイベント解除リクエストを受け取った
-- (BOOL)                           profile:(DConnectDeviceOrientationProfile *)profile
-didReceiveDeleteOnDeviceOrientationRequest:(DConnectRequestMessage *)request
-                                  response:(DConnectResponseMessage *)response
-                                  serviceId:(NSString *)serviceId
-                                sessionKey:(NSString *)sessionKey
-{
-	// DConnectイベント削除
-	[DPPebbleProfileUtil handleRequest:request response:response isRemove:YES callback:^{
-		// Pebbleのイベント削除
-		[[DPPebbleManager sharedManager] deleteDeviceOrientationEvent:serviceId callback:^(NSError *error) {
-            if (error) {
-                NSLog(@"Error:%@", error);
-            }
-		}];
-	}];
-	return YES;
 }
 
 @end
