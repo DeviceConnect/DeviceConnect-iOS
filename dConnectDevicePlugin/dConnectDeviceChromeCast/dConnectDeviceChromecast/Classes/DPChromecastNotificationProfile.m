83cfd0a9fee7635ecadd44508eaebfacb8828fd6
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2016-07-27 14:11:52 +0900

    ChromecastデバイスプラグインのNotificationProfileを新構造へ変更。

--- dConnectDevicePlugin/dConnectDeviceChromeCast/dConnectDeviceChromecast/Classes/DPChromecastNotificationProfile.m
+++ dConnectDevicePlugin/dConnectDeviceChromeCast/dConnectDeviceChromecast/Classes/DPChromecastNotificationProfile.m
@@ -22,6 +22,85 @@
     self = [super init];
     if (self) {
         self.delegate = self;
+        __weak DPChromecastNotificationProfile *weakSelf = self;
+        
+        // API登録(didReceivePostNotifyRequest相当)
+        NSString *postNotifyRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                        interfaceName: nil
+                                                        attributeName: DConnectNotificationProfileAttrNotify];
+        [self addPostPath: postNotifyRequestApiPath
+                      api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                          
+//                          NSData *icon = [DConnectNotificationProfile iconFromRequest:request];
+                          NSNumber *type = [DConnectNotificationProfile typeFromRequest:request];
+//                          NSString *dir = [DConnectNotificationProfile dirFromRequest:request];
+//                          NSString *lang = [DConnectNotificationProfile langFromRequest:request];
+                          NSString *body = [DConnectNotificationProfile bodyFromRequest:request];
+//                          NSString *tag = [DConnectNotificationProfile tagFromRequest:request];
+                          NSString *serviceId = [request serviceId];
+                          
+                          // パラメータチェック
+                          NSString *typeString = [request stringForKey:DConnectNotificationProfileParamType];
+                          
+                          if (!type || type.intValue < 0 || 3 < type.intValue
+                              || (type && ![[DPChromecastManager sharedManager] existDigitWithString:typeString])) {
+                              [response setErrorToInvalidRequestParameterWithMessage:@"type is null or invalid"];
+                              return YES;
+                          }
+                          if (!body) {
+                              [response setErrorToInvalidRequestParameterWithMessage:@"body is null"];
+                              return YES;
+                          }
+                          
+                          [DConnectNotificationProfile setNotificationId:@"dConnectDeviceChromeCast"
+                                                                  target:response];
+                          // リクエスト処理
+                          return [weakSelf handleRequest:request
+                                                response:response
+                                               serviceId:serviceId
+                                                callback:
+                                  ^{
+                                      // メッセージ送信
+                                      DPChromecastManager *mgr = [DPChromecastManager sharedManager];
+                                      [mgr sendMessageWithID:serviceId message:body type:[type intValue]];
+                                  }];
+                          
+                          return YES;
+                      }];
+
+        // API登録(didReceiveDeleteNotifyRequest相当)
+        NSString *deleteNotifyRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                          interfaceName: nil
+                                                          attributeName: DConnectNotificationProfileAttrNotify];
+        [self addDeletePath: deleteNotifyRequestApiPath
+                        api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                            
+                            NSString *serviceId = [request serviceId];
+                            NSString *notificationId = [DConnectNotificationProfile notificationIdFromRequest:request];
+                            
+                            if (!notificationId
+                                || (notificationId &&
+                                    ![notificationId isEqualToString:@"dConnectDeviceChromeCast"])) {
+                                    [response setErrorToInvalidRequestParameterWithMessage:@"NotificationId is invalid"];
+                                    return YES;
+                                } else if (notificationId &&
+                                           [notificationId isEqualToString:@"dConnectDeviceChromeCast"]) {
+                                    // リクエスト処理
+                                    return [weakSelf handleRequest:request
+                                                          response:response
+                                                         serviceId:serviceId
+                                                          callback:
+                                            ^{
+                                                // メッセージクリア
+                                                DPChromecastManager *mgr = [DPChromecastManager sharedManager];
+                                                [mgr clearMessageWithID:serviceId];
+                                            }];
+                                } else {
+                                    [response setErrorToUnknown];
+                                    return YES;
+                                }
+                        }];
+        
     }
     return self;
 }
@@ -51,82 +130,6 @@
         [[DConnectManager sharedManager] sendResponse:response];
     }];
     return NO;
-}
-
-
-
-#pragma mark - Post Methods
-
-// ノーティフィケーションの表示リクエストを受け取った
-- (BOOL)            profile:(DConnectNotificationProfile *)profile
-didReceivePostNotifyRequest:(DConnectRequestMessage *)request
-                   response:(DConnectResponseMessage *)response
-                   serviceId:(NSString *)serviceId
-                       type:(NSNumber *)type
-                        dir:(NSString *)dir
-                       lang:(NSString *)lang
-                       body:(NSString *)body
-                        tag:(NSString *)tag
-                       icon:(NSData *)icon
-{
-    // パラメータチェック
-    NSString *typeString = [request stringForKey:DConnectNotificationProfileParamType];
-
-    if (!type || type.intValue < 0 || 3 < type.intValue
-        || (type && ![[DPChromecastManager sharedManager] existDigitWithString:typeString])) {
-        [response setErrorToInvalidRequestParameterWithMessage:@"type is null or invalid"];
-        return YES;
-    }
-    if (!body) {
-        [response setErrorToInvalidRequestParameterWithMessage:@"body is null"];
-        return YES;
-    }
-    
-    [DConnectNotificationProfile setNotificationId:@"dConnectDeviceChromeCast"
-                                            target:response];
-    // リクエスト処理
-    return [self handleRequest:request
-                      response:response
-                      serviceId:serviceId
-                      callback:
-            ^{
-                // メッセージ送信
-                DPChromecastManager *mgr = [DPChromecastManager sharedManager];
-                [mgr sendMessageWithID:serviceId message:body type:[type intValue]];
-            }];
-}
-
-
-#pragma mark - Delete Methods
-
-// ノーティフィケーションの削除リクエストを受け取った
-- (BOOL)              profile:(DConnectNotificationProfile *)profile
-didReceiveDeleteNotifyRequest:(DConnectRequestMessage *)request
-                     response:(DConnectResponseMessage *)response
-                     serviceId:(NSString *)serviceId
-               notificationId:(NSString *)notificationId
-{
-    if (!notificationId
-        || (notificationId &&
-        ![notificationId isEqualToString:@"dConnectDeviceChromeCast"])) {
-        [response setErrorToInvalidRequestParameterWithMessage:@"NotificationId is invalid"];
-        return YES;
-    } else if (notificationId &&
-               [notificationId isEqualToString:@"dConnectDeviceChromeCast"]) {
-        // リクエスト処理
-        return [self handleRequest:request
-                          response:response
-                          serviceId:serviceId
-                          callback:
-                ^{
-                    // メッセージクリア
-                    DPChromecastManager *mgr = [DPChromecastManager sharedManager];
-                    [mgr clearMessageWithID:serviceId];
-                }];
-    } else {
-        [response setErrorToUnknown];
-        return YES;
-    }
 }
 
 @end
