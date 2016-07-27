093e4a78f28d07c056072ddcc1b14ff4ff6e9477
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2016-07-27 23:41:51 +0900

    ThetaデバイスプラグインのSystemProfileを新構造へ変更。

--- dConnectDevicePlugin/dConnectDeviceTheta/dConnectDeviceTheta/Classes/DPThetatSystemProfile.m
+++ dConnectDevicePlugin/dConnectDeviceTheta/dConnectDeviceTheta/Classes/DPThetatSystemProfile.m
@@ -32,6 +32,17 @@
         // イベントマネージャを取得
         self.eventMgr = [DConnectEventManager sharedManagerForClass:[DPThetaDevicePlugin class]];
         
+        // API登録(dataSourceのsettingPageForRequestを実行する処理を登録)
+        NSString *putSettingPageForRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                               interfaceName: DConnectSystemProfileInterfaceDevice
+                                                               attributeName: DConnectSystemProfileAttrWakeUp];
+        [self addPutPath: putSettingPageForRequestApiPath
+                     api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                         
+                         BOOL send = [weakSelf didReceivePutWakeupRequest:request response:response];
+                         return send;
+                     }];
+        
         // API登録(didReceiveDeleteEventsRequest相当)
         NSString *deleteEventsRequestApiPath = [self apiPathWithProfile: self.profileName
                                                           interfaceName: nil
