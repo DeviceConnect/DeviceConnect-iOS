cad33052e510de8f703650d3b19d27b003ffb051
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2016-07-27 23:47:47 +0900

    AlljoynデバイスプラグインのSystemProfileを新構造へ変更。

--- dConnectDevicePlugin/dConnectDeviceAllJoyn/dConnectDeviceAllJoyn/Sources/Profiles/DPAllJoynSystemProfile.m
+++ dConnectDevicePlugin/dConnectDeviceAllJoyn/dConnectDeviceAllJoyn/Sources/Profiles/DPAllJoynSystemProfile.m
@@ -25,6 +25,18 @@
     if (self) {
         self.dataSource = self;
         self.version = version;
+        __weak DPAllJoynSystemProfile *weakSelf = self;
+        
+        // API登録(dataSourceのsettingPageForRequestを実行する処理を登録)
+        NSString *putSettingPageForRequestApiPath = [self apiPathWithProfile: self.profileName
+                                                               interfaceName: DConnectSystemProfileInterfaceDevice
+                                                               attributeName: DConnectSystemProfileAttrWakeUp];
+        [self addPutPath: putSettingPageForRequestApiPath
+                     api:^BOOL(DConnectRequestMessage *request, DConnectResponseMessage *response) {
+                         
+                         BOOL send = [weakSelf didReceivePutWakeupRequest:request response:response];
+                         return send;
+                     }];
     }
     return self;
 }
