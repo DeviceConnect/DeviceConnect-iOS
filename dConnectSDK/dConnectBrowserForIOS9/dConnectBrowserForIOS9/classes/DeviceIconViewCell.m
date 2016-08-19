babc9b2d8ed7e9de2d6a4682a9af2ab23c526d3b
TakayukiHoshi1984 <hoshi@gclue.jp>
2016-08-19 17:06:36 +0900

    アイコンの透明度間隔の修正。

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewCell.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewCell.m
@@ -56,9 +56,12 @@
 }
 
 - (IBAction)didTouchDown:(UIButton *)sender {
-    [UIView animateWithDuration:0.15 animations:^{
-        self.alpha = 0.3;
-    }];
+    
+    __weak DeviceIconViewCell* _self = self;
+    self.alpha = 0.3;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        _self.alpha = 1.0;
+    });
 }
 
 - (IBAction)backNormal:(UIButton *)sender {
