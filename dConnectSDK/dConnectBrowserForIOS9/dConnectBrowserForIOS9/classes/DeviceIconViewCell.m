49369d16a2956972848aed6c7db53c0856d0199e
Tetsuya Hirano <hirano@hirano-dept.com>
2016-08-21 21:32:30 +0900

    Revert

--- dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewCell.m
+++ dConnectSDK/dConnectBrowserForIOS9/dConnectBrowserForIOS9/classes/DeviceIconViewCell.m
@@ -56,12 +56,9 @@
 }
 
 - (IBAction)didTouchDown:(UIButton *)sender {
-    
-    __weak DeviceIconViewCell* _self = self;
-    self.alpha = 0.3;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        _self.alpha = 1.0;
-    });
+    [UIView animateWithDuration:0.15 animations:^{
+        self.alpha = 0.3;
+    }];
 }
 
 - (IBAction)backNormal:(UIButton *)sender {
