c75e7fc3ca52c66c7c9a693c6f01a818f8295228
Masaru Takano <mtaka@gclue.jp>
2017-03-10 18:44:21 +0900

    curlコマンドでzipをダウンロードするように変更。
    wgetコマンドはmac標準ではないため、別途インストール手順が必要になってしまう。

--- dConnectSDK/dConnectSDKForIOS/download-spec.sh
+++ dConnectSDK/dConnectSDKForIOS/download-spec.sh
@@ -8,14 +8,16 @@ SPEC_ZIP_URL=https://github.com/TakayukiHoshi1984/DeviceConnect-Spec/archive/${S
 SPEC_DIR=${PROJECT_DIR}/DConnectSDK_resources/api
 
 if [ -e ${SPEC_DIR} ]; then
-    echo "Already created directory: ${SPEC_DIR}"
-    exit 1
+    rm -rf ${SPEC_DIR}
 fi
 
 mkdir -p ${SPEC_DIR}
 mkdir -p ${TEMP_DIR}
 cd ${TEMP_DIR}
-wget ${SPEC_ZIP_URL}
+
+echo "Download URL: ${SPEC_ZIP_URL}"
+curl -L -O -k ${SPEC_ZIP_URL}
+
 unzip -d . ${SPEC_ZIP_NAME}
 cp ${TEMP_DIR}/DeviceConnect-Spec-${REVISION}/api/*.json ${SPEC_DIR}
 rm -rf ${TEMP_DIR}
